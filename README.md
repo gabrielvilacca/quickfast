15830a52f3a0e1f33bcf4119e0cd1f47ec68e686
Rafael Pieroni <rafael@microsaasmilionario.com.br>
2023-10-18 19:33:21 -0300

    Main

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # React + Vite
 
-Function URL (onHotmartWebhook(us-central1)): https://us-central1-projeto-teste-405b3.cloudfunctions.net/onHotmartWebhook
-
-# projeto-teste
+# amazing-app
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,14 +5,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 // TODO: Colocar o firebaseConfig do seu app aqui abaixo
-const firebaseConfig = {
-  apiKey: "AIzaSyAGHixNK8LcQsQVlq0ahKE8lkKWI9KzN5M",
-  authDomain: "projeto-teste-405b3.firebaseapp.com",
-  projectId: "projeto-teste-405b3",
-  storageBucket: "projeto-teste-405b3.appspot.com",
-  messagingSenderId: "306468732119",
-  appId: "1:306468732119:web:148ed7ea0bfe3495828492",
-};
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
